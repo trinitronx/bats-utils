f972244168d2a2bbc04c93bcc2bed8765039acd0
James Cuzella <james.cuzella@returnpath.com>
2016-06-21 02:06:39 -0600

    Adding y2j.bash + install_y2j()
    Change-Id: I4e92042c250879e76c935acd00f30a48de799dcf

---
+++ src/y2j.bash
@@ -0,0 +1,3 @@
+install_y2j() {
+  docker run --rm wildducktheories/y2j y2j.sh installer /usr/local/bin | bash &>/dev/null || fail "Could not install test helper: y2j via docker bash script install method.  Tests cannot proceed."
+}
