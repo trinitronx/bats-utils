6b9bedf81a9c192a9f558de5e54f056b3967a612
James Cuzella <james.cuzella@returnpath.com>
2016-08-17 11:36:22 -0600

    Switch version of y2j used to my fork with merged PR wildducktheories/y2j#5
    Change-Id: I1d384734d4870e011e0ea8c6bce71374596bc35f

--- src/y2j.bash
+++ src/y2j.bash
@@ -1,3 +1,3 @@
 install_y2j() {
-  docker run --rm wildducktheories/y2j y2j.sh installer /usr/local/bin | bash &>/dev/null || fail "Could not install test helper: y2j via docker bash script install method.  Tests cannot proceed."
+  docker run --rm trinitronx/y2j:develop y2j.sh installer /usr/local/bin | bash &>/dev/null || fail "Could not install test helper: y2j via docker bash script install method.  Tests cannot proceed."
 }
